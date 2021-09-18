7c84515fa083b45991bd1fd0a3322e841ac08a45
dixithimanshu <dixit.himanshu.1205@gmail.com>
2021-09-18 23:30:00 +0530

    Final Commit

---
+++ Multiply.java
@@ -0,0 +1,8 @@
+public class Multiply {
+
+    public static void main(String arg[]) {
+        int a = 5, b = 4;
+        int mul = a * b;
+        System.out.println("Multiply : " + mul);
+    }
+}
